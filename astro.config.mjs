68b3ee0aac8c5188999ce1f53a517dfb6a460191
AdithyaLaakso <adithya.laakso@gmail.com>
2025-08-29 16:42:31 -0400

    added root path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adidev.site',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
