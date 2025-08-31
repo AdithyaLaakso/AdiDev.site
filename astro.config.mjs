6a0caf6638d4b1639352ce087c8d22eb2410eada
AdithyaLaakso <adithya.laakso@gmail.com>
2025-08-31 18:38:49 -0400

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import "./src/styles/global.css"
 // https://astro.build/config
 export default defineConfig({
   // site: 'AdiDev.site',
-  base: '/dist',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
