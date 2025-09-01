22f8e232efcc30da38edc2937ba8dfc2507260a7
AdithyaLaakso <adithya.laakso@gmail.com>
2025-08-31 21:22:21 -0400

    minor grammer fix

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const siteConfig = {
     github: "https://github.com/AdithyaLaakso",
   },
   aboutMe:
-    "I am a well well-rounded full-stack developer with strong additional interests in computer vision and DevOps. In my free-time I like to play chess and write fantasy novels",
+    "I am a well-rounded full-stack developer with strong additional interests in computer vision and DevOps. In my free-time I like to play chess and write fantasy novels",
   skills: ["TypeScript", "C++", "React", "Node.js", "Python", "Shell Scripting", "PyTorch", "Rust"],
   projects: [
     {
